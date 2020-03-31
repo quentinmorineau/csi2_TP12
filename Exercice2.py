e862e949f38d2857d27b08023f998361369e9269
quentinmorineau <quentin.morineau@isen-ouest.yncrea.fr>
2020-03-31 12:15:44 +0200

    .

--- Exercice2.py
+++ Exercice2.py
@@ -3,7 +3,7 @@ def reverseString(s, i = 0) :
    """Reverses the string s recursively """
    if i == len(s):
        return ""
-   return reverseString(s, i+1)
+   return reverseString(s, i+1)+s[i]
 
 print(reverseString("")) # ""
 print(reverseString("bonjour")) # ruojnob
